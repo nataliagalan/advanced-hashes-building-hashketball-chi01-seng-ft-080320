e4c5538bed643725477e0bea852276dc198d552c
Natalia Galán <n8galan@gmail.com>
2020-07-09 03:14:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -56,20 +56,10 @@ players_data = {
         :steals => 4,
         :blocks => 11,
         :slam_dunks => 1 },
+        
     ]
    },
 
-   let(:jason_terry) {
-     { :player_name => "Jason Terry",
-       :number => 31,
-       :shoe => 15,
-       :points => 19,
-       :rebounds => 2,
-       :assists => 2,
-       :steals => 4,
-       :blocks => 11,
-       :slam_dunks => 1 },
-   }
 
 
 
