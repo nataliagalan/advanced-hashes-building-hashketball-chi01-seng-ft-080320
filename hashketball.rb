301c0398fe4d55784a938c0482c62caec067669b
Natalia Galán <n8galan@gmail.com>
2020-07-09 03:41:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -83,14 +83,14 @@ players_data = {
         :blocks => 15,
         :slam_dunks => 10 },
 
-      { :player_name => "DeSagna Diop", 
-        :number => 2, 
-        :shoe => 14, 
-        :points => 24, 
-        :rebounds => 12, 
-        :assists => 12, 
-        :steals => 4, 
-        :blocks => 5, 
+      { :player_name => "DeSagna Diop",
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
         :slam_dunks => 5 },
 
     ]
