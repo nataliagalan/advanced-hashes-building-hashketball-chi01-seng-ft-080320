9532ee41111f6c59e071da37cea924a469dffbb9
Natalia Galán <n8galan@gmail.com>
2020-07-09 03:37:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,11 +63,24 @@ players_data = {
     :team_name => "Charlotte Hornets",
     :colors => ["Turquoise", "Purple"],
     :players => [
-
+      { :player_name => "Jeff Adrien", 
+        :number => 4, 
+        :shoe => 18, 
+        :points => 10, 
+        :rebounds => 1, 
+        :assists => 1, 
+        :steals => 2, 
+        :blocks => 7, 
+        :slam_dunks => 2 },
 
     ]
   }
  }
+
+
+
+
+
 
 players_data
 end
