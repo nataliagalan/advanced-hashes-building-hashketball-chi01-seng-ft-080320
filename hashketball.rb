4dfde94605c68b32df4667382e85bd08d235ffe9
Natalia Galán <n8galan@gmail.com>
2020-07-09 03:06:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -10,17 +10,17 @@ players_data = {
    },
 
   #  let(:alan_anderson) {
-  #    { :player_name => "Alan Anderson", 
-  #      :number => 0, :shoe => 16, 
-  #      :points => 22, :rebounds => 12, 
-  #      :assists => 12, 
-  #      :steals => 3, 
-  #      :blocks => 1, 
+  #    { :player_name => "Alan Anderson",
+  #      :number => 0, :shoe => 16,
+  #      :points => 22, :rebounds => 12,
+  #      :assists => 12,
+  #      :steals => 3,
+  #      :blocks => 1,
   #      :slam_dunks => 1 }
   #  }
 
 
-   
+
   :away => {
     :team_name => " ",
     :colors => [" ", " "],
