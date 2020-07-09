57b7658915cf106292cfdf6640bf52950dc837fd
Natalia Galán <n8galan@gmail.com>
2020-07-09 03:12:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -36,24 +36,14 @@ players_data = {
         :steals => 3,
         :blocks => 1,
         :slam_dunks => 15 },
+        
 
     ]
    },
 
 
 
-   let(:brook_lopez) {
-     { :player_name => "Brook Lopez",
-       :number => 11,
-       :shoe => 17,
-       :points => 17,
-       :rebounds => 19,
-       :assists => 10,
-       :steals => 3,
-       :blocks => 1,
-       :slam_dunks => 15 },
-   }
-
+  
 
 
   :away => {
