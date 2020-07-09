37a7ce1f3397a8e1e274b68e629cf2f261e07639
Natalia Galán <n8galan@gmail.com>
2020-07-09 03:06:47 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,14 +6,25 @@ players_data = {
   :home => {
     :team_name => "Brooklyn Nets",
     :colors => ["Black", "White"],
-    :players => {
-
-    }
+    :players => [ ]
    },
+
+  #  let(:alan_anderson) {
+  #    { :player_name => "Alan Anderson", 
+  #      :number => 0, :shoe => 16, 
+  #      :points => 22, :rebounds => 12, 
+  #      :assists => 12, 
+  #      :steals => 3, 
+  #      :blocks => 1, 
+  #      :slam_dunks => 1 }
+  #  }
+
+
+   
   :away => {
     :team_name => " ",
     :colors => [" ", " "],
-    :players => { }
+    :players => []
   }
  }
 
