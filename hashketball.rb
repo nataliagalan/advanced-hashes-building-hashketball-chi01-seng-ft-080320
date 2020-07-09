c0fe29bb91a2e16115c7f4ee73d6ed6d5b0dc960
Natalia Galán <n8galan@gmail.com>
2020-07-09 03:42:52 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -93,15 +93,15 @@ players_data = {
         :blocks => 5,
         :slam_dunks => 5 },
 
-        { :player_name => "Ben Gordon",
-          :number => 8,
-          :shoe => 15,
-          :points => 33,
-          :rebounds => 3,
-          :assists => 2,
-          :steals => 1,
-          :blocks => 1,
-          :slam_dunks => 0 },
+      { :player_name => "Ben Gordon",
+        :number => 8,
+        :shoe => 15,
+        :points => 33,
+        :rebounds => 3,
+        :assists => 2,
+        :steals => 1,
+        :blocks => 1,
+        :slam_dunks => 0 },
 
     ]
   }
