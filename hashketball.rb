a1833a5a2d8b988a813866ddd0ab3bd4b117a976
Natalia Galán <n8galan@gmail.com>
2020-07-09 03:11:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -17,22 +17,42 @@ players_data = {
         :blocks => 1,
         :slam_dunks => 1 },
 
-        { :player_name => "Reggie Evans",
-          :number => 30,
-          :shoe => 14,
-          :points => 12,
-          :rebounds => 12,
-          :assists => 12,
-          :steals => 12,
-          :blocks => 12,
-          :slam_dunks => 7 },
+      { :player_name => "Reggie Evans",
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7 },
+
+      { :player_name => "Brook Lopez", 
+        :number => 11, 
+        :shoe => 17, 
+        :points => 17, 
+        :rebounds => 19, 
+        :assists => 10, 
+        :steals => 3, 
+        :blocks => 1, 
+        :slam_dunks => 15 },
 
     ]
    },
 
 
 
-
+   let(:brook_lopez) {
+     { :player_name => "Brook Lopez", 
+       :number => 11, 
+       :shoe => 17, 
+       :points => 17, 
+       :rebounds => 19, 
+       :assists => 10, 
+       :steals => 3, 
+       :blocks => 1, 
+       :slam_dunks => 15 },
+   }
 
 
 
