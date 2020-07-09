e52b535ef3c65c5526391ababbbcf7aa02234a0d
Natalia Galán <n8galan@gmail.com>
2020-07-09 03:39:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -89,17 +89,7 @@ players_data = {
 
 
 
- let(:bismack_biyombo) {
-   { :player_name => "Bismack Biyombo",
-     :number => 0,
-     :shoe => 16,
-     :points => 12,
-     :rebounds => 4,
-     :assists => 7,
-     :steals => 22,
-     :blocks => 15,
-     :slam_dunks => 10 },
- }
+
 
 
 
